f0e37a771c0c3ee7f3675473ff118aaa4e2ad27a
Mathias Conradt <mathias.conradt@sonarsource.com>
2025-09-09 20:04:22 +0200

    sonar-scan.sh & workflow

---
+++ sonar-scan.sh
@@ -0,0 +1,5 @@
+mvn clean verify sonar:sonar \
+  -Dsonar.projectKey=e-corp-demo_JavaCoffeeShop_a0509c22-d2a1-4904-83a4-a5ec14211ea3 \
+  -Dsonar.projectName='JavaCoffeeShop' \
+  -Dsonar.host.url=https://mathiasconradt.ngrok.io \
+  -Dsonar.branch.name=main
