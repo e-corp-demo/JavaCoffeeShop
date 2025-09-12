081e4ead02a519696656b660af38d12e3a44860a
Mathias Conradt <mathias.conradt@sonarsource.com>
2025-09-12 10:37:10 +0200

    sonar-scan.sh

--- sonar-scan.sh
+++ sonar-scan.sh
@@ -1,5 +1,14 @@
+# Get the current branch name directly using command substitution
+CURRENT_BRANCH=$(git branch --show-current)
+
+# Check if the command was successful and a branch name was found
+if [ -z "$CURRENT_BRANCH" ]; then
+    echo "Error: Could not determine the current Git branch."
+    exit 1
+fi
+
 mvn clean verify sonar:sonar \
   -Dsonar.projectKey=e-corp-demo_JavaCoffeeShop_a0509c22-d2a1-4904-83a4-a5ec14211ea3 \
   -Dsonar.projectName='JavaCoffeeShop' \
   -Dsonar.host.url=https://mathiasconradt.ngrok.io \
-  -Dsonar.branch.name=main
+  -Dsonar.branch.name=$CURRENT_BRANCH
